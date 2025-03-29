958ca26d2c501812c5442080eb268dc1b10d018a
davidspiv <daspivack@gmail.com>
2025-03-28 17:12:23 -0700

    extracted logic from main

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,23 @@ struct Color {
   int b;
 };
 
+// convert sRGB channel to a linear value
+double linearizeRGB(int channel) {
+  double normalizedRGB = channel / 255.0;
+
+  if (normalizedRGB <= 0.04045) {
+    return normalizedRGB / 12.92;
+  } else {
+    return pow(((normalizedRGB + 0.055) / 1.055), 2.4);
+  }
+}
+
+double distSquared(const Color &colorA, const Color &colorB) {
+  const double rD = colorB.r - colorA.r;
+  const double gD = colorB.g - colorA.g;
+  const double bD = colorB.b - colorA.b;
+  return rD * rD + gD * gD + bD * bD;
+}
 
 Color getAverageRGB(const Picture &pic) {
   // https://sighack.com/post/averaging-rgb-colors-the-right-way
@@ -38,9 +55,7 @@ Color getAverageRGB(const Picture &pic) {
           int(std::round(sqrt(b / numPx)))};
 }
 
-
-int main() {
-  std::string dir = "./blocks";
+std::vector<std::string> getValidPaths(std::string dir) {
   std::vector<std::string> fPaths;
 
   for (const auto &f : std::filesystem::directory_iterator(dir)) {
@@ -52,8 +67,10 @@ int main() {
     }
   }
 
-  //   fPaths.push_back("blocks/calibrated_sculk_sensor_amethyst.png");
+  return fPaths;
+}
 
+std::vector<Picture> getValidTextures(std::vector<std::string> fPaths) {
   const size_t numTiles = fPaths.size();
   std::vector<Picture> validTextures;
 
@@ -79,7 +96,40 @@ int main() {
     }
   }
 
-  // STITCH VALID TILES
+  return validTextures;
+}
+
+std::vector<Color>
+buildQuantizedColors(const std::vector<Picture> &validTextures) {
+  const size_t numValidTiles = validTextures.size();
+  std::vector<Color> quantColors(numValidTiles, Color());
+
+  for (size_t i = 0; i < numValidTiles; i++) {
+    Picture pic(validTextures[i]);
+    Color averageRGB = getAverageRGB(pic);
+    quantColors.at(i) = averageRGB;
+  }
+
+  return quantColors;
+}
+
+int findNearestColor(const Color &target,
+                     const std::vector<Color> &quantColors) {
+  int nearest = 0;
+  double minDist = std::numeric_limits<double>::max();
+
+  for (size_t i = 0; i < quantColors.size(); i++) {
+    double dist = distSquared(target, quantColors.at(i));
+    if (dist < minDist) {
+      minDist = dist;
+      nearest = i;
+    }
+  }
+
+  return nearest;
+}
+
+void buildAtlas(const std::vector<Picture> &validTextures) {
   const int tileSize = 16;
   const size_t numValidTiles = validTextures.size();
   const int gridSize =
@@ -94,20 +144,54 @@ int main() {
     int xOffset = (i % gridSize) * tileSize; // Column position
     int yOffset = (i / gridSize) * tileSize; // Row position
 
-    Color averageRGB = getAverageRGB(pic);
-
     for (size_t j = 0; j < tileSize; j++) {
       for (size_t k = 0; k < tileSize; k++) {
-        // const int r = pic.red(k, j);
-        // const int g = pic.green(k, j);
-        // const int b = pic.blue(k, j);
+        const int r = pic.red(k, j);
+        const int g = pic.green(k, j);
+        const int b = pic.blue(k, j);
 
-        auto [rA, gA, bA] = averageRGB;
-
-        atlas.set(xOffset + k, yOffset + j, rA, gA, bA, 255);
+        atlas.set(xOffset + k, yOffset + j, r, g, b, 255);
       }
     }
   }
 
   atlas.save("atlas.png");
+}
+
+void createTestPic(const Color &exactColor,
+                   const std::vector<Picture> &validTextures,
+                   const std::vector<Color> &quantColors) {
+
+  auto [exactR, exactG, exactB] = exactColor;
+  Picture testPic(32, 16, exactR, exactG, exactB);
+
+  const int texIdx = findNearestColor({exactR, exactG, exactB}, quantColors);
+
+
+  for (int j = 0; j < testPic.height(); j++) {
+    for (int i = testPic.width() / 2; i < testPic.width(); i++) {
+
+
+      int r = validTextures.at(texIdx).red(i - (testPic.width() / 2), j);
+      int g = validTextures.at(texIdx).green(i - (testPic.width() / 2), j);
+      int b = validTextures.at(texIdx).blue(i - (testPic.width() / 2), j);
+
+      testPic.set(i, j, r, g, b, 255);
+    }
+  }
+
+  testPic.save("testPic.png");
+}
+
+int main() {
+  const std::string dir = "./blocks";
+
+  const std::vector<std::string> fPaths = getValidPaths(dir);
+  const std::vector<Picture> validTextures = getValidTextures(fPaths);
+  const std::vector<Color> quantColors = buildQuantizedColors(validTextures);
+
+  const Color testColor = {167, 118, 96};
+  createTestPic(testColor, validTextures, quantColors);
+
+  //   buildAtlas(validTextures);
 }
