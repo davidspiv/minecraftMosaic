1753c804d1e72510cd7723cf91b32eac2db487e5
davidspiv <daspivack@gmail.com>
2025-03-30 21:11:38 -0700

    textured working

--- src/util.cpp
+++ src/util.cpp
@@ -22,6 +22,35 @@ multiplyMatrix(const std::array<std::array<double, 3>, 3> &matrix,
 }
 
 
+// StdRGB getAverageRGB(const Picture &pic, int originX, int originY) {
+//   // https://sighack.com/post/averaging-rgb-colors-the-right-way
+
+//   double r = 0;
+//   double b = 0;
+//   double g = 0;
+
+//   int numPx = 0;
+//   for (int j = originY; j < originY + 16; j++) {
+//     for (int i = originX; i < originX + 16; i++) {
+
+//       const double rComponent = pic.red(i, j);
+//       const double gComponent = pic.green(i, j);
+//       const double bComponent = pic.blue(i, j);
+
+//       /* Sum the squares of components instead */
+//       r += rComponent * rComponent;
+//       g += gComponent * gComponent;
+//       b += bComponent * bComponent;
+
+//       ++numPx;
+//     }
+//   }
+//   /* Return the sqrt of the mean of squared R, G, and B sums */
+//   return {int(std::round(sqrt(r / numPx))), int(std::round(sqrt(g / numPx))),
+//           int(std::round(sqrt(b / numPx)))};
+// }
+
+
 StdRGB getAverageRGB(const Picture &pic, int originX, int originY) {
   // https://sighack.com/post/averaging-rgb-colors-the-right-way
   double lStar = 0;
@@ -36,9 +65,9 @@ StdRGB getAverageRGB(const Picture &pic, int originX, int originY) {
       const CieLab cieLabComponent(stdRGB);
 
       /* Sum the squares of components */
-      lStar += cieLabComponent.lStar * cieLabComponent.lStar;
-      aStar += cieLabComponent.aStar * cieLabComponent.aStar;
-      bStar += cieLabComponent.bStar * cieLabComponent.bStar;
+	  lStar += cieLabComponent.lStar;
+	  aStar += cieLabComponent.aStar;
+	  bStar += cieLabComponent.bStar;
 
       ++numPx;
     }
@@ -47,10 +76,9 @@ StdRGB getAverageRGB(const Picture &pic, int originX, int originY) {
   if (numPx == 0)
     return StdRGB(0, 0, 0); // Prevent division by zero
 
-  /* Compute the root mean square */
-  lStar = std::sqrt(lStar / numPx);
-  aStar = std::sqrt(aStar / numPx);
-  bStar = std::sqrt(bStar / numPx);
+  lStar /= numPx;
+  aStar /= numPx;
+  bStar /= numPx;
 
   const CieLab cieLabFinal(lStar, aStar, bStar);
   return StdRGB(cieLabFinal);
